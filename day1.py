eba37b60b671c49af0ff10894a0a76308175d98e
shaik-tanveer-dev <tanveer.shaik2026@gmail.com>
2026-02-13 20:16:38 +0530

    Add files via upload
    Introduction to Python

---
+++ day1.py
@@ -0,0 +1,17 @@
+# Day 1 — Introduction to Python
+# Author: Shaik Tanveer
+# Training: Codegnan Python Full Stack
+
+print("Hello! Welcome to my Day 1 Python Program")
+
+
+print("Hi")
+print("This is Tanveer from Codegnan Python Full Stack Training.")
+
+print("Today I learned:")
+print("Introduction to Python")
+print("Installation of Python")
+print("Basic concepts of Python")
+
+print(" Thank you for running my program!")
+print("See you in Day 2 ")

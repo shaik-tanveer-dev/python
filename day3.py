b9b15266257cf20b5cf114052231c8fde6791ef8
shaik-tanveer-dev <tanveer.shaik2026@gmail.com>
2026-02-15 21:15:08 +0530

    Add files via upload
    Day3@codeganan

---
+++ day3.py
@@ -0,0 +1,27 @@
+#Day3
+"""#check if number is zero
+
+num=int(input())
+
+if num==0:
+    print("Number is zero")
+print("program done") """
+
+
+"""#check if number is zero or not
+num=int(input())
+if num==0:
+    print("Number is zero")
+else:
+    print("not zero")
+print("program done")"""
+
+
+#read 2 numbers find bigger one
+num1=int(input())
+num2=int(input())
+
+if num1>num2:
+    print("num1 is bigger")
+else:
+    print("num2 is bigger")

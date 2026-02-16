9231d12a7fd0e35121983c7b2ed54af72a040382
shaik-tanveer-dev <tanveer.shaik2026@gmail.com>
2026-02-16 18:54:53 +0530

    Add files via upload
    Day4@codeganan

---
+++ day4.py
@@ -0,0 +1,8 @@
+#min_project on voting system for the backend
+age=int(input("Enter a age: "))
+if age==18 and age>18:
+    print("Eligible for the vote")
+elif age<18:
+    print("Not eligible for the vote")
+else:
+    print("Enter a valide age")

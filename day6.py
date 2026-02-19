06aa30bffe3c675e69ad2f493a7cd116ee1cbad7
shaik-tanveer-dev <tanveer.shaik2026@gmail.com>
2026-02-19 15:02:35 +0530

    Add files via upload
    Day6@codegnan

---
+++ day6.py
@@ -0,0 +1,66 @@
+'''a = 10
+b = 20
+c = 15
+if a%2==0:
+    print(f"Even {a}")
+else:
+    print(f"odd {a}")
+if b%2==0:
+    print(f"even {b}")
+else:
+    print(f"odd {b}")
+if c%2==0:
+    print(f"even {c}")
+else:
+    print(f"odd {c}")'''
+
+
+'''li=[1,2,3,4,5]
+for i in range(0,5,1):
+    if li[i]%2==0:
+        print("even")
+    else:
+        print("odd")'''
+
+'''#loops
+#1.print even numbers between 20 - 40
+for i in range(20,41,2):
+    print(i)'''
+
+'''#2.
+for i in range(20,41,1):
+    if i%2==0:
+        print(i)'''
+
+'''for i in range(1,101,1):
+    print(i , end = ",")'''
+
+'''#sum of n natural  numbers
+n=int(input("Enter a value: "))
+total  = 0
+for i in range(1,n+1):
+    total = total + i
+print(total)'''
+
+'''num = 1
+while num<101:
+    print(num)
+    num+=1'''
+
+'''li = [10,15,20,25,30]
+i = 0
+while i<len(li):
+    if li[i]%2==0:
+        print(f"even {li[i]}")
+    else:
+        print(f"odd {li[i]}")
+    i+=1'''
+
+'''n = int(input())
+i = 0
+s = 0
+while i<n:
+    if i%2==0:
+        s+=i
+    i+=1
+print(s)'''
